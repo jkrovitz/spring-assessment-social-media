733d46d0c3937da15e1d6b054644c22d2a2c21da
NickW <nicholas.watson@mailfence.com>
2021-10-19 08:57:32 -0700

    Added hashtag and user entity

--- src/main/java/com/cooksys/socialmedia/socialmedia/entities/Hashtag.java
+++ src/main/java/com/cooksys/socialmedia/socialmedia/entities/Hashtag.java
@@ -1,5 +1,40 @@
 package com.cooksys.socialmedia.socialmedia.entities;
 
+import java.sql.Timestamp;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Data
 public class Hashtag {
-	
+
+    @Id
+    @GeneratedValue
+    @Column(name = "hashtag_id")
+    private Long id;
+
+    @Column(unique = true)
+    private String label;
+
+    private Timestamp firstUsed;
+
+    private Timestamp lastUsed;
+
+    @ManyToMany
+    @JoinTable(
+            name = "tweet_hashtag",
+            joinColumns = @JoinColumn(name = "hashtag_id"),
+            inverseJoinColumns = @JoinColumn(name = "tweet_id"))
+    private List<Tweet> tweets;
 }
--- src/main/java/com/cooksys/socialmedia/socialmedia/entities/User.java
+++ src/main/java/com/cooksys/socialmedia/socialmedia/entities/User.java
@@ -3,9 +3,9 @@ package com.cooksys.socialmedia.socialmedia.entities;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.List;
 
 @Entity
 @NoArgsConstructor
@@ -15,11 +15,27 @@ public class User {
     @Id
     @GeneratedValue
     private Long id;
+
+    @Column(unique = true)
     private String username;
     private String password;
-    private long timestamp;
+    private Timestamp joined;
     private String firstName;
     private String lastName;
     private String email;
     private String phone;
+
+    //DOUBLE CHECK RELATIONSHIPS
+    @OneToMany(mappedBy = "user")
+    private List<Tweet> tweets;
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_tweets",
+            joinColumns = @JoinColumn(name = "tweet_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id"))
+    private List<Tweet> likedTweets;
+    private List<User> followers; //NEEDS TO FIX RELATIONSHIP
+    private List<User> following; //NEEDS TO FIX RELATIONSHIP
+    private List<Tweet> mentions;
 }
