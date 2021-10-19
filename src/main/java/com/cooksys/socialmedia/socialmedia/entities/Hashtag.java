e8f45dbe50723d03b7c5c85363b2bffd8a363443
Jeremy Krovitz <jkrovitz@JeremyWliamComp.hitronhub.home>
2021-10-18 21:33:15 -0500

    Code Hashtag entity class

--- src/main/java/com/cooksys/socialmedia/socialmedia/entities/Hashtag.java
+++ src/main/java/com/cooksys/socialmedia/socialmedia/entities/Hashtag.java
@@ -1,5 +1,40 @@
 package com.cooksys.socialmedia.socialmedia.entities;
 
+import java.sql.Timestamp;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Data
 public class Hashtag {
 	
+	@Id
+	@GeneratedValue
+	@Column(name="hashtag_id")
+	private Long id;
+	
+	@Column(unique=true)
+	private String label; // is there a way to indicate that this field should be case insensitive?
+	
+	private Timestamp firstUsed; // how do I go about preventing this column from being updated after creation?
+	
+	private Timestamp lastUsed;
+	
+	@ManyToMany
+	@JoinTable(
+			name = "tweet_hashtag",
+			joinColumns = @JoinColumn(name="hashtag_id"),
+			inverseJoinColumns = @JoinColumn(name="tweet_id"))
+	private List<Tweet> tweets;
 }
