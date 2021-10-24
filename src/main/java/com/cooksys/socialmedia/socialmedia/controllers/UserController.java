6574f65f68117333264f194f24a22fa436fc6073
Jeremy Krovitz <jkrovitz@JeremyWliamComp.hitronhub.home>
2021-10-23 19:26:02 -0500

    Review and/or revise 3 endpoints * DELETE users/@{username} * POST users/@{username}/follow * POST users/@{username}/unfollow

--- src/main/java/com/cooksys/socialmedia/socialmedia/controllers/UserController.java
+++ src/main/java/com/cooksys/socialmedia/socialmedia/controllers/UserController.java
@@ -2,15 +2,22 @@ package com.cooksys.socialmedia.socialmedia.controllers;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
-import com.cooksys.socialmedia.socialmedia.dtos.CredentialsDto;
 import com.cooksys.socialmedia.socialmedia.dtos.TweetResponseDto;
 import com.cooksys.socialmedia.socialmedia.dtos.UserRequestDto;
 import com.cooksys.socialmedia.socialmedia.dtos.UserResponseDto;
 import com.cooksys.socialmedia.socialmedia.entities.Tweet;
 import com.cooksys.socialmedia.socialmedia.services.UserService;
-
-import org.springframework.web.bind.annotation.*;
 
 import lombok.RequiredArgsConstructor;
 
@@ -23,10 +30,10 @@ public class UserController {
 
     @GetMapping
     public List<UserResponseDto> getAllUsers() {
-
         return userService.getAllUsers();
     }
 
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public UserResponseDto createUser(@RequestBody UserRequestDto userRequestDto) {
         return userService.createUser(userRequestDto);
@@ -49,11 +56,13 @@ public class UserController {
     }
 
     @PostMapping("/@{username}/follow")
+    @ResponseStatus(HttpStatus.CREATED)
     public void followUser(@PathVariable String username, @RequestBody UserRequestDto userRequestDto) {
         userService.followUser(username, userRequestDto);
     }
 
     @PostMapping("/@{username}/unfollow")
+    @ResponseStatus(HttpStatus.CREATED)
     public void unFollowUser(@PathVariable String username, @RequestBody UserRequestDto userRequestDto) {
         userService.unFollowUser(username, userRequestDto);
     }
--- src/main/java/com/cooksys/socialmedia/socialmedia/services/impl/UserServiceImpl.java
+++ src/main/java/com/cooksys/socialmedia/socialmedia/services/impl/UserServiceImpl.java
@@ -31,32 +31,52 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
-    private final UserRepository userRepository;
-    private final TweetRepository tweetRepository;
-    private final UserMapper userMapper;
-    private final TweetMapper tweetMapper;
+	private final UserRepository userRepository;
+	private final TweetRepository tweetRepository;
+	private final UserMapper userMapper;
+	private final TweetMapper tweetMapper;
 
-    //CALLBACK/HELPER METHODS
-    private void checkUser(User user, Credentials credentials) {
-        if (!user.getCredentials().equals(credentials)) {
-            throw new BadRequestException("The credentials are not valid.");
-        }
-    }
+	// CALLBACK/HELPER METHODS
+	private void checkUser(User user, Credentials credentials) {
+		if (!user.getCredentials().equals(credentials)) {
+			throw new BadRequestException("The credentials are not valid.");
+		}
+	}
 
-    private User getUserByUsername(String username) {
-        Optional<User> optionalUser = userRepository.findByCredentialsUsername(username);
-        if (optionalUser.isEmpty()) {
-            throw new NotFoundException("No user found.");
-        }
-        User user = optionalUser.get();
-        if (user.isDeleted()) {
-        	throw new BadRequestException("User has been flagged as deleted.");
-        }
-        return user;
-    }
+	private boolean checkUserBool(User user, Credentials credentials) {
+		if (!user.getCredentials().equals(credentials)) {
+			throw new BadRequestException("The credentials are not valid.");
+		}
+		return true;
+	}
 
-    
-    @Override
+	private User getUserByUsername(String username) {
+		Optional<User> optionalUser = userRepository.findByCredentialsUsername(username);
+		if (optionalUser.isEmpty()) {
+			throw new NotFoundException("No user found.");
+		}
+		User user = optionalUser.get();
+//		if (user.isDeleted()) {
+//			throw new BadRequestException("User has been flagged as deleted.");
+//		}
+		return user;
+	}
+
+	private User deleteUserByUsernameHelper(String username) {
+		Optional<User> optionalUser = userRepository.findByCredentialsUsername(username);
+		if (optionalUser.isEmpty()) {
+
+			throw new NotFoundException("The user to delete was not found.");
+		}
+		User user = optionalUser.get();
+		if (user.isDeleted()) {
+			throw new BadRequestException("The user has already been flagged as deleted.");
+		}
+		return user;
+
+	}
+
+	@Override
 	public List<UserResponseDto> getAllUsers() {
 		List<User> userList = new ArrayList<User>();
 		for (User user : userRepository.findAll((Sort.by(Sort.Direction.DESC, "joined")))) {
@@ -66,139 +86,145 @@ public class UserServiceImpl implements UserService {
 		}
 		return userMapper.entitiesToDtos(userList);
 	}
-    
-    @Override
-    public UserResponseDto createUser(UserRequestDto userRequestDto) {
-        String username = userRequestDto.getCredentials().getUsername();
-        Optional<User> user = userRepository.findByCredentialsUsername(username);
-        if (user.isPresent() && user.get().isDeleted() &&
-                user.get().getCredentials().getPassword().equals(userRequestDto.getCredentials().getPassword())) {
-        	java.sql.Timestamp joined = user.get().getJoined();
-        	user.get().setDeleted(false);
-            user.get().setJoined(joined);
-            return userMapper.entityToDto(userRepository.saveAndFlush(user.get()));
-        } else if ((user.isPresent()) && (user.get().getCredentials().getPassword().equals(userRequestDto.getCredentials().getPassword()))) {
-            throw new BadRequestException("The username is not available.");
-        } else {
-            return userMapper.entityToDto(userRepository.saveAndFlush(userMapper.dtoToEntity(userRequestDto)));
-        }
-    }
 
-    //ADD CATCHES & REFACTOR
-    @Override
-    public UserResponseDto getUserUsername(String username) {
-        return userMapper.entityToDto(getUserByUsername(username));
-    }
+	@Override
+	public UserResponseDto createUser(UserRequestDto userRequestDto) {
+		String username = userRequestDto.getCredentials().getUsername();
+		Optional<User> user = userRepository.findByCredentialsUsername(username);
+		if (user.isPresent() && user.get().isDeleted()
+				&& user.get().getCredentials().getPassword().equals(userRequestDto.getCredentials().getPassword())) {
+			java.sql.Timestamp joined = user.get().getJoined();
+			user.get().setDeleted(false);
+			user.get().setJoined(joined);
+			return userMapper.entityToDto(userRepository.saveAndFlush(user.get()));
+		} else if ((user.isPresent())
+				&& (user.get().getCredentials().getPassword().equals(userRequestDto.getCredentials().getPassword()))) {
+			throw new BadRequestException("The username is not available.");
+		} else {
+			return userMapper.entityToDto(userRepository.saveAndFlush(userMapper.dtoToEntity(userRequestDto)));
+		}
+	}
 
-    //ADD CATCHES & REFACTOR
-    @Override
-    public UserResponseDto deleteUser(String username, UserRequestDto userRequestDto) {
-        User user = getUserByUsername(userRequestDto.getCredentials().getUsername());
-        User check = userMapper.dtoToEntity(userRequestDto);
-        checkUser(user, check.getCredentials());
-        User userToDelete = new User();
-        for (User aUser : userRepository.findAll()) {
-        	if ((aUser.getCredentials().getUsername()).equals(username)) {
-        		userToDelete = aUser;
-        	}
-        }
-        userToDelete.setDeleted(true);
-        return userMapper.entityToDto(userRepository.saveAndFlush(userToDelete));
-    }
+	// ADD CATCHES & REFACTOR
+	@Override
+	public UserResponseDto getUserUsername(String username) {
+		return userMapper.entityToDto(getUserByUsername(username));
+	}
 
-    //ADD CATCHES & REFACTOR
-    @Override
-    public UserResponseDto userNameProfileUpdate(String username, UserRequestDto userRequestDto) {
-        User user = getUserByUsername(userRequestDto.getCredentials().getUsername());
-        User check = userMapper.dtoToEntity(userRequestDto);
-        checkUser(user, check.getCredentials());
-        user.setProfile(check.getProfile());
-        Credentials credentials = check.getCredentials();
-        for (User aUser : userRepository.findAll()) {
-        	if (aUser.getCredentials().getUsername().equals(username)) {
+	@Override
+	public UserResponseDto deleteUser(String username, UserRequestDto userRequestDto) {
+		User userWithCredentialsToCompare = deleteUserByUsernameHelper(userRequestDto.getCredentials().getUsername());
+		String usernameOfUserWithCredentialsToCompare = userWithCredentialsToCompare.getCredentials().getUsername();
+		String passwordOfUserWithCredentialsToCompare = userWithCredentialsToCompare.getCredentials().getPassword();
+		Optional<User> optionalUser = userRepository.findByCredentialsUsername(username);
+		if (optionalUser.isPresent()) {
+			User userToDelete = optionalUser.get();
+			String userToDeleteUsername = username;
+			String userToDeletePassword = userToDelete.getCredentials().getPassword();
+			if ((usernameOfUserWithCredentialsToCompare.equals(userToDeleteUsername))
+					&& (passwordOfUserWithCredentialsToCompare.equals(userToDeletePassword))) {
+				userToDelete.setDeleted(true);
+				return userMapper.entityToDto(userRepository.saveAndFlush(userToDelete));
+			} else {
+				throw new BadRequestException("Credentials do not match.");
+			}
+
+		} else {
+			throw new NotFoundException("The user to delete was not found.");
+		}
+
+	}
+
+	// ADD CATCHES & REFACTOR
+	@Override
+	public UserResponseDto userNameProfileUpdate(String username, UserRequestDto userRequestDto) {
+		User user = getUserByUsername(userRequestDto.getCredentials().getUsername());
+		User check = userMapper.dtoToEntity(userRequestDto);
+		checkUser(user, check.getCredentials());
+		user.setProfile(check.getProfile());
+		Credentials credentials = check.getCredentials();
+		for (User aUser : userRepository.findAll()) {
+			if (aUser.getCredentials().getUsername().equals(username)) {
 				throw new BadRequestException("A user with that username already exists in the database.");
 			}
-        		
+
 		}
-        credentials.setUsername(username);
-        user.setCredentials(credentials);    
-        return userMapper.entityToDto(userRepository.saveAndFlush(user));
-    }
+		credentials.setUsername(username);
+		user.setCredentials(credentials);
+		return userMapper.entityToDto(userRepository.saveAndFlush(user));
+	}
 
-    @Override
-    public void followUser(String username, UserRequestDto userRequestDto) {
-        User follower = getUserByUsername(userRequestDto.getCredentials().getUsername());
-        User check = userMapper.dtoToEntity(userRequestDto);
-        checkUser(follower, check.getCredentials());
-        User followee = getUserByUsername(username);
-        if (!followee.getFollowers().contains(follower)) {
-            follower.userFollow(followee);
-            followee.userFollowing(follower);
-            userRepository.saveAndFlush(followee);
-            userRepository.saveAndFlush(follower);
-        } else {
-            throw new BadRequestException("Already following user");
-        }
-    }
+	@Override
+	public void followUser(String username, UserRequestDto userRequestDto) {
+		User follower = getUserByUsername(userRequestDto.getCredentials().getUsername());
+		User check = userMapper.dtoToEntity(userRequestDto);
+		checkUser(follower, check.getCredentials());
+		User followee = getUserByUsername(username);
+		if (!followee.getFollowers().contains(follower)) {
+			follower.userFollow(followee);
+			followee.userFollowing(follower);
+			userRepository.saveAndFlush(followee);
+			userRepository.saveAndFlush(follower);
+		} else {
+			throw new BadRequestException("Already following user");
+		}
+	}
 
+	@Override
+	public void unFollowUser(String username, UserRequestDto userRequestDto) {
+		User follower = getUserByUsername(userRequestDto.getCredentials().getUsername());
+		User check = userMapper.dtoToEntity(userRequestDto);
+		checkUser(follower, check.getCredentials());
+		User followee = getUserByUsername(username);
+		if (followee.getFollowers().contains(follower)) {
+			follower.userUnfollow(followee);
+			followee.userUnfollowing(follower);
+			userRepository.saveAndFlush(followee);
+		} else {
+			throw new BadRequestException("You're already not following user");
+		}
+	}
 
-    @Override
-    public void unFollowUser(String username, UserRequestDto userRequestDto) {
-        User follower = getUserByUsername(userRequestDto.getCredentials().getUsername());
-        User check = userMapper.dtoToEntity(userRequestDto);
-        checkUser(follower, check.getCredentials());
-        User followee = getUserByUsername(username);
-        if (followee.getFollowers().contains(follower)) {
-            follower.userUnfollow(followee);
-            followee.userUnfollowing(follower);
-            userRepository.saveAndFlush(followee);
-        } else {
-            throw new BadRequestException("You're already not following user");
-        }
-    }
+	@Override
+	public List<UserResponseDto> getUserFollowers(String username) {
+		User user = getUserByUsername(username);
+		List<User> followers = user.getFollowers();
+		return userMapper.entitiesToDtos(followers);
+	}
 
-    @Override
-    public List<UserResponseDto> getUserFollowers(String username) {
-        User user = getUserByUsername(username);
-        List<User> followers = user.getFollowers();
-        return userMapper.entitiesToDtos(followers);
-    }
+	@Override
+	public List<UserResponseDto> getUserFollowing(String username) {
+		User user = getUserByUsername(username);
+		List<User> following = user.getFollowing();
+		return userMapper.entitiesToDtos(following);
+	}
 
-    @Override
-    public List<UserResponseDto> getUserFollowing(String username) {
-        User user = getUserByUsername(username);
-        List<User> following = user.getFollowing();
-        return userMapper.entitiesToDtos(following);
-    }
+	@Override
+	public List<TweetResponseDto> getUserTweets(String username) {
+		return tweetRepository.findByDeletedFalseAndAuthorOrderByPostedDesc(username);
+	}
 
-    @Override
-    public List<TweetResponseDto> getUserTweets(String username) {
-        return tweetRepository.findByDeletedFalseAndAuthorOrderByPostedDesc(username);
-    }
+	@Override
+	public List<TweetResponseDto> getUserMentions(String username) {
+		User user = getUserByUsername(username);
+		List<Tweet> userMentions = user.getMentions();
+		return tweetMapper.entitiesToDtos(userMentions);
+	}
 
-    @Override
-    public List<TweetResponseDto> getUserMentions(String username) {
-        User user = getUserByUsername(username);
-        List<Tweet> userMentions = user.getMentions();
-        return tweetMapper.entitiesToDtos(userMentions);
-    }
+	@Override
+	public List<Tweet> getUserFeed(String username) {
+		User tweetUser = getUserByUsername(username);
+		if (tweetUser.getIsActive().equals(true)) {
+			Set<User> tweetUsers = tweetUser.getUserFollowing().stream().filter(user -> user.getIsActive().equals(true))
+					.collect(Collectors.toSet());
+			tweetUsers.add(tweetUser);
+			List<Tweet> tweetU = new ArrayList<>();
 
-    @Override
-    public List<Tweet> getUserFeed(String username) {
-        User tweetUser = getUserByUsername(username);
-        if (tweetUser.getIsActive().equals(true)) {
-            Set<User> tweetUsers = tweetUser.getUserFollowing().stream()
-                    .filter(user -> user.getIsActive().equals(true))
-                    .collect(Collectors.toSet());
-            tweetUsers.add(tweetUser);
-            List<Tweet> tweetU = new ArrayList<>();
-
-            for(User tweetUser2 : tweetUsers) {
-                tweetU.addAll(tweetUser2.getTweets());
-            }
-            return tweetU;
-        }
-        throw new EntityNotFoundException();
-    }
+			for (User tweetUser2 : tweetUsers) {
+				tweetU.addAll(tweetUser2.getTweets());
+			}
+			return tweetU;
+		}
+		throw new EntityNotFoundException();
+	}
 }
-
